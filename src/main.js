9a83ca51ef9fb094228b2cfeb66b96cf4ce05cf8
mindaaaa <avalc735@gmail.com>
2025-03-02 13:26:08 +0900

    refactor: 필요없는 콘솔로그 제거

--- src/main.js
+++ src/main.js
@@ -3,7 +3,6 @@
 // Header에 페이지 아래로 스크롤시 다크 스타일링 적용
 const header = document.querySelector('.header');
 const headerHeight = header.getBoundingClientRect().height;
-console.log(headerHeight);
 document.addEventListener('scroll', ()=>{
   if(window.scrollY > headerHeight){
     header.classList.add('header--dark')
