833087d3af66640311cf0b85f95aec161a08a083
mindaaaa <avalc735@gmail.com>
2025-03-03 15:59:07 +0900

    feat: typeit을 이용한 타이핑 효과 추가

---
+++ src/type.js
@@ -0,0 +1,20 @@
+'use strict';
+
+new TypeIt('.home__title--strong', {loop: true, speed: 150,}) 
+.move(-8)
+.type('HONG ')
+.pause(1000)
+.move(null, {to: 'END'})
+.delete()
+.type('Front-end Engineer')
+.pause(1000)
+.move(-9)
+.delete(9)
+.type('Back-end')
+.pause(1000)
+.delete(8)
+.type('Full-stack')
+.pause(1000)
+.move(9)
+.delete()
+.go();
